2fb8c949152dda16621a6f4051a1eb79a120d8f5
Ernesto Jiménez Villaseñor <netorcido@gmail.com>
2014-12-19 11:55:37 -0600

    Más modificaciones de legibilidad

--- app/views/front/static/aliados.blade.php
+++ app/views/front/static/aliados.blade.php
@@ -4,70 +4,97 @@
 
 <div class="container">
 
-<section id="socios-nav">
-  <div class="row clearfix">
-    <div class="col-md-12 column">
-      <h3 class="text-left">Socios de capacitación</h3>
-    </div>
-  </div>
-</section>
+	<section id="socios-nav">
 
-<section id="socios-contenido">
-  <div class="row clearfix">
-    <div class="col-md-12 column">
-      <div class="row">
-      <div class="col-md-3 column">
-          <a href=""><img src="img/adm.png" alt=""></a>
-      </div>
-      <div class="col-md-3 column">
-          <a href=""><img src="img/kmmx.png" alt=""></a>
-      </div>
-      <div class="col-md-3 column">
-          <a href=""><img src="img/aula-virtual.png" alt=""></a>
-      </div>
-      <div class="col-md-3 column">
-          <a href=""><img src="img/adobe.png" alt=""></a>
-      </div>
-      </div>
-    </div>
-    <div class="col-md-12 column">
-       <div class="row">
-      <div class="col-md-3 column">
-          <a href=""><img src="img/edumac.png" alt=""></a>
-      </div>
-      <div class="col-md-3 column">
-          <a href=""><img src="img/infotec.png" alt=""></a>
-      </div>
-      <div class="col-md-3 column">
-          <a href=""><img src="img/arca.png" alt=""></a>
-      </div>
-      <div class="col-md-3 column">
-          <a href=""><img src="img/cayab.png" alt=""></a>
-      </div>
-    </div>
-          <div class="row">
-      <div class="col-md-3 column">
-          <a href=""><img src="img/adm.png" alt=""></a>
-      </div>
-      <div class="col-md-3 column">
-          <a href=""><img src="img/kmmx.png" alt=""></a>
-      </div>
-      <div class="col-md-3 column">
-          <a href=""><img src="img/aula-virtual.png" alt=""></a>
-      </div>
-      <div class="col-md-3 column">
-          <a href=""><img src="img/adobe.png" alt=""></a>
-      </div>
-      </div>
-    </div>
-  </div>
-    <div class="row clearfix">
-    <div class="col-md-12 column">
-              <button class="btn text-right">Convertirme en socio</button>
-              <button class="btn text-right">Ayuda</button>
-    </div>
-    </div> 
-</section>
+		<div class="row clearfix">
+			<div class="col-md-12 column">
+				<h3 class="text-left">Socios de capacitación</h3>
+			</div>
+		</div>
+
+	</section>
+
+	<section id="socios-contenido">
+
+		<div class="row clearfix">
+
+			<div class="col-md-12 column">
+				<div class="row">
+
+					<div class="col-md-3 column">
+						<a href=""><img src="img/adm.png" alt=""></a>
+					</div>
+
+					<div class="col-md-3 column">
+						<a href=""><img src="img/kmmx.png" alt=""></a>
+					</div>
+
+					<div class="col-md-3 column">
+						<a href=""><img src="img/aula-virtual.png" alt=""></a>
+					</div>
+
+					<div class="col-md-3 column">
+						<a href=""><img src="img/adobe.png" alt=""></a>
+					</div>
+
+				</div>
+			</div>
+
+			<div class="col-md-12 column">
+
+				<div class="row">
+
+					<div class="col-md-3 column">
+						<a href=""><img src="img/edumac.png" alt=""></a>
+					</div>
+
+					<div class="col-md-3 column">
+						<a href=""><img src="img/infotec.png" alt=""></a>
+					</div>
+
+					<div class="col-md-3 column">
+						<a href=""><img src="img/arca.png" alt=""></a>
+					</div>
+
+					<div class="col-md-3 column">
+						<a href=""><img src="img/cayab.png" alt=""></a>
+					</div>
+
+				</div>
+
+				<div class="row">
+
+					<div class="col-md-3 column">
+						<a href=""><img src="img/adm.png" alt=""></a>
+					</div>
+
+					<div class="col-md-3 column">
+						<a href=""><img src="img/kmmx.png" alt=""></a>
+					</div>
+
+					<div class="col-md-3 column">
+						<a href=""><img src="img/aula-virtual.png" alt=""></a>
+					</div>
+
+					<div class="col-md-3 column">
+						<a href=""><img src="img/adobe.png" alt=""></a>
+					</div>
+
+				</div>
+
+			</div>
+
+		</div>
+
+		<div class="row clearfix">
+			<div class="col-md-12 column">
+				<button class="btn text-right">Convertirme en socio</button>
+				<button class="btn text-right">Ayuda</button>
+			</div>
+		</div>
+
+	</section>
+
 </div>
 
 @stop
