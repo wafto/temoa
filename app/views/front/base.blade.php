4ee845374fd2ae66cad9c77490b71d3c6fde4cd6
Ernesto Jiménez Villaseñor <netorcido@gmail.com>
2015-01-06 19:00:27 -0600

    Se quitan modals del template base

--- app/views/front/base.blade.php
+++ app/views/front/base.blade.php
@@ -134,34 +134,6 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 	<script src="/js/bootstrap.min.js"></script>
 
-
-        <script>
-$(document).ready(function(){
-    $("#myModal").modal("toggle");
-})
-        </script>
-
-	<!-- Modals -->
-
-	<!-- Login -->
-<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="false">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-        <h4 class="modal-title" id="myModalLabel">Modal title</h4>
-      </div>
-      <div class="modal-body">
-        ...
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
-      </div>
-    </div>
-  </div>
-</div>
-
     @yield('scripts-bottom')
 	</body>
 </html>
